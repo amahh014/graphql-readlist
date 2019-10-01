017da6d16b054dcb95095f8219e2effdb8bad62d
amahh014 <nizammamedovv@gmail.com>
2019-10-01 21:41:44 +0200

    initial commit, nizam's reading list

---
+++ server/app.js
@@ -0,0 +1,24 @@
+const express = require ('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require ('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+//allow cross-origin requests
+app.use(cors());
+
+mongoose.connect('mongodb+srv://new_user:salam123@amahh014-8blyw.mongodb.net/test?retryWrites=true&w=majority' , { useNewUrlParser: true })
+mongoose.connection.once('open',() => {
+    console.log('connected to database');
+});
+
+app.use('/graphql',graphqlHTTP({
+   schema,
+   graphiql: true
+}));
+
+app.listen(4000,()=> {
+    console.log('now listening for requests on port 4000')
+});
